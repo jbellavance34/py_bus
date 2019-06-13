3e35a6fb32c46bafa1bf2c04122a9690118a5616
brap <jbellavance34@gmail.com>
2019-06-13 00:16:28 -0400

    api fait et test suite

--- pybus.py
+++ pybus.py
@@ -4,68 +4,80 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from requests import ConnectionError
+from flask import request, url_for
+from flask_api import FlaskAPI, status, exceptions
+from flask import jsonify
 
-# Fetch the html file
-try:
-    RESPONSE = urllib.request.urlopen('http://www.ville.saint-jean-sur-richelieu.qc.ca/transport-en-commun/Documents/horaires/96.html', timeout=10)
-    HTML_DOC = RESPONSE.read()
-except Exception as E:
-    print("Exception is :" + E)
+app = FlaskAPI(__name__)
 
-# Parse the html file
-SOUP = BeautifulSoup(HTML_DOC, 'html.parser')
+@app.route("/", methods=['GET'])
+def parse_bus():
+    if request.method == 'GET':
+        try:
+            RESPONSE = urllib.request.urlopen('http://www.ville.saint-jean-sur-richelieu.qc.ca/transport-en-commun/Documents/horaires/96.html', timeout=10)
+            HTML_DOC = RESPONSE.read()
+        except Exception as E:
+            print("Exception is :" + E)
 
-DIR_LIST = SOUP.find_all('div', attrs={"id" : "div-horaires"})
-DIR_TO_MTRL_TABLE = DIR_LIST[0].find('table')
-DIR_FROM_MTRL_TABLE = DIR_LIST[3].find('table')
+        # Parse the html file
+        SOUP = BeautifulSoup(HTML_DOC, 'html.parser')
 
-SPEED_TO_MTRL = DIR_TO_MTRL_TABLE.find_all('div', attrs={"align" : "center"})
-START_TO_MTRL = DIR_TO_MTRL_TABLE.find_all('tr')[1]
-END_TO_MTRL = DIR_TO_MTRL_TABLE.find_all('tr')[-1]
+        DIR_LIST = SOUP.find_all('div', attrs={"id" : "div-horaires"})
+        DIR_TO_MTRL_TABLE = DIR_LIST[0].find('table')
+        DIR_FROM_MTRL_TABLE = DIR_LIST[3].find('table')
 
-SPEED_TO_SJSR = DIR_FROM_MTRL_TABLE.find_all('div', attrs={"align" : "center"})
-START_TO_SJSR = DIR_FROM_MTRL_TABLE.find_all('tr')[1]
-END_TO_SJSR = DIR_FROM_MTRL_TABLE.find_all('tr')[-1]
+        SPEED_TO_MTRL = DIR_TO_MTRL_TABLE.find_all('div', attrs={"align" : "center"})
+        START_TO_MTRL = DIR_TO_MTRL_TABLE.find_all('tr')[1]
+        END_TO_MTRL = DIR_TO_MTRL_TABLE.find_all('tr')[-1]
 
-START_TO_MTRL_LST = []
-END_TO_MTRL_LST = []
-START_TO_SJSR_LST = []
-END_TO_SJSR_LST = []
-# filter the return and populate list MTRL
+        SPEED_TO_SJSR = DIR_FROM_MTRL_TABLE.find_all('div', attrs={"align" : "center"})
+        START_TO_SJSR = DIR_FROM_MTRL_TABLE.find_all('tr')[1]
+        END_TO_SJSR = DIR_FROM_MTRL_TABLE.find_all('tr')[-1]
 
-def populate_list_direction(city_start, list_start, city_end, list_end):
-    for item in city_start:
-        item = str(item)
-        if item != '\n':
-            list_start.append(re.sub("<.*?>", "", item))
-    for item in city_end:
-        item = str(item)
-        if item != '\n':
-            list_end.append(re.sub("<.*?>", "", item))
-populate_list_direction(START_TO_MTRL, START_TO_MTRL_LST, END_TO_MTRL, END_TO_MTRL_LST)
-populate_list_direction(START_TO_SJSR, START_TO_SJSR_LST, END_TO_SJSR, END_TO_SJSR_LST)
+        START_TO_MTRL_LST = []
+        END_TO_MTRL_LST = []
+        START_TO_SJSR_LST = []
+        END_TO_SJSR_LST = []
+        # filter the return and populate list MTRL
 
-# todo, prendre en charge caractere unicode pour les jours ferier
-# https://www.fileformat.info/info/unicode/char/2600/index.htm
+        def populate_list_direction(city_start, list_start, city_end, list_end):
+            for item in city_start:
+                item = str(item)
+                if item != '\n':
+                    list_start.append(re.sub("<.*?>", "", item))
+            for item in city_end:
+                item = str(item)
+                if item != '\n':
+                    list_end.append(re.sub("<.*?>", "", item))
+        populate_list_direction(START_TO_MTRL, START_TO_MTRL_LST, END_TO_MTRL, END_TO_MTRL_LST)
+        populate_list_direction(START_TO_SJSR, START_TO_SJSR_LST, END_TO_SJSR, END_TO_SJSR_LST)
 
-for speed, start, end in zip(SPEED_TO_MTRL, START_TO_MTRL_LST, END_TO_MTRL_LST):
-    if "S" in speed.text:
-        speed_long = "Super Express"
-    if "E" in speed.text:
-        speed_long = "Express      "
-    if "L" in speed.text:
-        speed_long = "Locale       "
-    if "A" in speed.text:
-        speed_long = "Autoroute 30 "
-    print("Autobus destination MTRL - Vitesse: " + speed_long + " Depart:" + start + " Arriver:" + end)
+        # todo, prendre en charge caractere unicode pour les jours ferier
+        # https://www.fileformat.info/info/unicode/char/2600/index.htm
+        complete_return_value = []
+        for speed, start, end in zip(SPEED_TO_MTRL, START_TO_MTRL_LST, END_TO_MTRL_LST):
+            if "S" in speed.text:
+                speed_long = "Super Express"
+            if "E" in speed.text:
+                speed_long = "Express      "
+            if "L" in speed.text:
+                speed_long = "Locale       "
+            if "A" in speed.text:
+                speed_long = "Autoroute 30 "
+            complete_return_value.append("Autobus destination MTRL - Vitesse: " + speed_long + " Depart:" + start + " Arriver:" + end)
 
-for speed, start, end in zip(SPEED_TO_SJSR, START_TO_SJSR_LST, END_TO_SJSR_LST):
-    if "S" in speed.text:
-        speed_long = "Super Express"
-    if "E" in speed.text:
-        speed_long = "Express      "
-    if "L" in speed.text:
-        speed_long = "Locale       "
-    if "A" in speed.text:
-        speed_long = "Autoroute 30 "
-    print("Autobus destination SJSR - Vitesse: " + speed_long + " Depart:" + start + " Arriver:" + end)
+        for speed, start, end in zip(SPEED_TO_SJSR, START_TO_SJSR_LST, END_TO_SJSR_LST):
+            if "S" in speed.text:
+                speed_long = "Super Express"
+            if "E" in speed.text:
+                speed_long = "Express      "
+            if "L" in speed.text:
+                speed_long = "Locale       "
+            if "A" in speed.text:
+                speed_long = "Autoroute 30 "
+            complete_return_value.append("Autobus destination SJSR - Vitesse: " + speed_long + " Depart:" + start + " Arriver:" + end)
+
+        return complete_return_value, status.HTTP_200_OK
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pybus_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import unittest
+from pybus import app
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True 
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
